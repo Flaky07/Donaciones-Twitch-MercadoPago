dadb9fb0aaadf9fa8602f4d2388fa7c2981e02f2
Flaky <f.orihuela00@gmail.com>
2025-04-09 15:19:35 -0300

    Set_Env_Front

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-const TWITCH_CHANNEL = "elmariscal_infinito"; // INDICAR NOMBRE DEL CANAL
-const BACKEND_URL = "https://backend-donaciones.onrender.com"; // URL DE LA API BACKEND EN RENDER.COM
+const TWITCH_CHANNEL = window.ENV?.TWITCH_CHANNEL || "default_channel"; // INDICAR NOMBRE DEL CANAL
+const BACKEND_URL = window.ENV?.BACKEND_URL || "https://backend-donaciones.onrender.com"; // URL DE LA API BACKEND EN RENDER.COM
